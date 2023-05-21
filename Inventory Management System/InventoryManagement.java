cb3e9d4c6e22ebc32007e79c766b98154caee133
ImShawnTheSheep <shawnmichaeljumawan@gmail.com>
2023-05-21 19:25:51 +0800

    Changed InventoryManagement Class to public

--- Inventory Management System/InventoryManagement.java
+++ Inventory Management System/InventoryManagement.java
@@ -4,7 +4,7 @@ class Inventory {
 
 }
 
-class InventoryManagement {
+public class InventoryManagement {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
