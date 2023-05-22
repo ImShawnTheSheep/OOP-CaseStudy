bd163de436fc62eb33bc484f60e05b5c026a81d5
keiii0 <kylakeithibaledoya@iskolarngbayan.pup.edu.ph>
2023-05-22 22:19:15 +0800

    Update

--- Library Management System/LibraryManagement.java
+++ Library Management System/LibraryManagement.java
@@ -8,7 +8,5 @@ class Book {
 
 public class LibraryManagement {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }
